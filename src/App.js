802a6e87489367961ef8ef24677e7946481381d9
AVT Estudos <andretamayoshi@gmail.com>
2020-04-18 19:10:04 -0300

    Corrigindo desafio

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <div>
-      <ul>
+      <ul data-testid="repository-list">
         { 
           repositories.map(repository => (
             <li key={repository.id}>
